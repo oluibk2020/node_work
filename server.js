8f05a179a3f2e6cedddad74b5f7a9636b2132e3e
Olugbenga Taiwo <taiwoolugbenga2018@gmail.com>
2023-11-09 13:06:52 +0100

    initial commit

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const _ = require('lodash')
+
 function addingMac(a, b) {
     return a +b
 }
 
-console.log(addingMac(3, 5));
+console.log(addingMac(3, 5))
+
+const studs = [
+    {id: 1, name: 'Ahmed Sodiq'},
+    {id: 2, name: 'Dominion Alabi'},
+    {id:3, name: "Treasue Aiyedun"}
+]
+
+console.log(_.pick(studs[0], ['name']));
